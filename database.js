fe16ad8e5b055a041d59ff125d6add19ee28d3da
Max Willamy <166521121+Maxwillamy777@users.noreply.github.com>
2025-04-05 14:12:31 -0300

    Corrigido erro de binário do better-sqlite3 para Render

--- database.js
+++ database.js
@@ -3,22 +3,18 @@ const Database = require('better-sqlite3');
 const db = new Database('./data/db.sqlite');
 
 // Cria a tabela se não existir
-db.serialize(() => {
-  db.run(`
-    CREATE TABLE IF NOT EXISTS usuarios (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      senha TEXT NOT NULL
-    )
-  `);
+db.exec(`
+  CREATE TABLE IF NOT EXISTS usuarios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    senha TEXT NOT NULL
+  );
 
-  db.run(`
-    CREATE TABLE IF NOT EXISTS mensagens (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      usuario_id INTEGER NOT NULL,
-      texto TEXT NOT NULL,
-      FOREIGN KEY(usuario_id) REFERENCES usuarios(id)
-    )
-  `);
-});
+  CREATE TABLE IF NOT EXISTS mensagens (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    usuario_id INTEGER NOT NULL,
+    texto TEXT NOT NULL,
+    FOREIGN KEY(usuario_id) REFERENCES usuarios(id)
+  );
+`);
 
 module.exports = db;
